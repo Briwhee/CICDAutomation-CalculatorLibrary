6f8028ff35baa72aca13077f624f72c24fe4ef7e
Briwhee <43559191+Briwhee@users.noreply.github.com>
2023-11-21 11:03:36 -0700

    Add multiplication for Calculator

--- test_calculator.py
+++ test_calculator.py
@@ -11,6 +11,9 @@ class TestCalculator:
     def test_subtraction(self):
         assert 2 == calculator.subtract(4,2)
 
+    def test_multiplication(self):
+        assers 100 == calculator.multiply(10,10)
+
 
 # test with "pytest -v --cov" this will test any file with the prefix test 
 # -v gives a clean look, 
