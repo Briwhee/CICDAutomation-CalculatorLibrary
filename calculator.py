7cbcf0a13f66633b69c9634f68d1f00218d4e9ad
Briwhee <43559191+Briwhee@users.noreply.github.com>
2023-11-22 21:41:11 -0700

    add new files

---
+++ calculator.py
@@ -0,0 +1,12 @@
+# Calculator library for CICD Practice
+
+def add(first_num, second_num):
+    return first_num + second_num
+
+
+def subtract(first_num, second_num):
+    return first_num - second_num
+
+
+def multiply(first_num, second_num):
+    return first_num * second_num
